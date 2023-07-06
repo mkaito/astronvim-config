9c72edad3d2afed7b608d17d5eaaf965f5d5f10c
Christian Höppner <chris@mkaito.net>
2023-07-06 19:40:20 +0100

    Reorganize mappings

--- mappings.lua
+++ mappings.lua
@@ -8,28 +8,25 @@ return {
   n = {
     -- second key is the lefthand side of the map
     [";"] = { ":", desc = "Command mode" },
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader><leader>"] = { "<cmd>Telescope buffers<cr>", desc = "Buffer list" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
-    ["<leader><BS>"] = { "<cmd>bdelete!<cr>", desc = "Close current buffer" },
+
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader><leader>"] = { "<cmd>Telescope buffers<cr>", desc = "Buffer list" },
+    ["<leader><BS>"] = { "<cmd>bdelete<cr>", desc = "Close current buffer" },
+
     -- Resume last Telescope session
-    ["<leader>sr"] = {
+    ["<leader>f<space>"] = {
       function() require("telescope.builtin").resume() end,
       desc = "Resume last Telescope search",
     },
-    ["<TAB>"] = { "<cmd>bprevious<cr>", desc = "Previous buffer" },
-    ["<S-TAB>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+
+    ["<TAB>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+    ["<S-TAB>"] = { "<cmd>bprevious<cr>", desc = "Previous buffer" },
+
     ["<leader>gx"] = { "<cmd>GitConflictListQf<CR>", desc = "Git conflicts" },
+
     ["<CR>"] = { "<cmd>NeoZoomToggle<CR>", desc = "Zoom in/out", nowait = true },
   },
   v = {
