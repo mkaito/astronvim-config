1cf442c66006744162c63435b3bafa6aaa5be5d0
Christian Höppner <chris@mkaito.net>
2023-07-12 21:54:01 +0100

    rust: give rust-analyzer its own profile

--- init.lua
+++ init.lua
@@ -75,6 +75,16 @@ return {
           },
         },
       },
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+          },
+        },
+      },
       -- pylsp = {
       --   settings = {
       --     pylsp = {
