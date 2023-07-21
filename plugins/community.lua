5e7496e6f081e1089c9f289ac85c0d799eab437a
Christian Höppner <chris@mkaito.net>
2023-07-21 14:56:08 +0100

    Add smartcolumn.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -37,4 +37,12 @@ return {
       },
     },
   },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 99,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
